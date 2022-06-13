5e7b765b2cd4f1ee9110326a715a73aeee84435a
acatasic <96747833+acatasic@users.noreply.github.com>
2022-06-14 01:40:24 +0200

    izmene i poboljsnja, jedino nisam ubacio da se selektuje vise stvari u checkboxu

--- Template/Controllers/IspitController.cs
+++ Template/Controllers/IspitController.cs
@@ -35,28 +35,122 @@ namespace Template.Controllers
 
         [Route("Poruci/{idProdavnice}/{idProizvoda}/{izabranaKolicina}")]
         [HttpPut]
-        public async Task<JsonResult> Poruci(int idProdavnice,int idProizvoda,int izabranaKolicina){
+        public async Task<ActionResult> Poruci(int idProdavnice,int idProizvoda,int izabranaKolicina){
 
 
-            var nadjeniNedostajuciSastojci=
+            /*var nadjeniNedostajuciSastojci=
             await Context.Sastojak.Include(p=>p.ProizvodSastojak).Where(p=>p.ProizvodSastojak.Any(q=>q.Proizvod.ID==idProizvoda))
-                                                                .Include(p=>p.ProdavnicaSastojak).Where(p=>p.ProdavnicaSastojak.Any((t=>t.Prodavnica.ID==idProdavnice && t.Kolicina<izabranaKolicina))).ToListAsync();
+                                .Include(p=>p.ProdavnicaSastojak).Where(p=>p.ProdavnicaSastojak.Any((t=>t.Prodavnica.ID==idProdavnice && t.Kolicina<izabranaKolicina))).ToListAsync();*/
 
-///vraca proizvode kojih nema dovoljno na zalihama
+            ///vraca proizvode kojih nema dovoljno na zalihama
            
-           /* var nadjeniProizvodSastojak=await Context.ProizvodSastojak.Include(p=>p.Sastojak).Where(p=>p.Proizvod.ID==idProizvoda).ToListAsync();
+            var nadjeniProizvodSastojak=await Context.ProizvodSastojak.Include(p=>p.Sastojak).Where(p=>p.Proizvod.ID==idProizvoda).ToListAsync();
 
-            List<string> listaZaVracanje= new "";
+            List<ProizvodSastojak> listaZaVracanje = new List<ProizvodSastojak>();///obrati paznju na definisanje lista
 
             foreach(ProizvodSastojak element in nadjeniProizvodSastojak){
                 var nadjenaKolicinaSastojkaUFrizideru=await Context.ProdavnicaSastojak.Include(p=>p.Sastojak).Where(p=>p.Prodavnica.ID==idProdavnice && p.Sastojak.ID==element.Sastojak.ID).FirstAsync();
                                
                 if (izabranaKolicina*element.Kolicina > nadjenaKolicinaSastojkaUFrizideru.Kolicina)
-                 listaZaVracanje.Add(nadjenaKolicinaSastojkaUFrizideru.Sastojak.Naziv);//return new JsonResult(nadjeniProizvodSastojak);
-            }
-*/
+                 listaZaVracanje.Add(element);//return new JsonResult(nadjeniProizvodSastojak);
 
-         return new JsonResult(nadjeniNedostajuciSastojci);
+                 else nadjenaKolicinaSastojkaUFrizideru.Kolicina-=izabranaKolicina*element.Kolicina;
+                 Context.ProdavnicaSastojak.Update(nadjenaKolicinaSastojkaUFrizideru);
+                 await Context.SaveChangesAsync();
+
+            }
+            if (listaZaVracanje!=null){
+            return Ok(listaZaVracanje);}
+            else return BadRequest("Nije bilo kupovine");
+
         }
+
+        [Route("MenadzerPorucuje/{idProdavnice}/{idSastojka}")]
+        [HttpPut]
+        public async Task<ActionResult> MenadzerPorucuje(int idProdavnice,int idSastojka){
+
+            var nadjeniProdavnicaSastojak=await Context.ProdavnicaSastojak.Where(p=>p.Prodavnica.ID==idProdavnice && p.Sastojak.ID==idSastojka).FirstAsync();
+            nadjeniProdavnicaSastojak.Kolicina+=300;
+            Context.ProdavnicaSastojak.Update(nadjeniProdavnicaSastojak);
+            await Context.SaveChangesAsync();
+            return Ok("azurirana vrednost u bazi "+ idSastojka);
+        }
+
+
     }
 }
+/*
+[
+  {
+    "id": 1,
+    "kolicina": 100,
+    "sastojak": {
+      "id": 1,
+      "naziv": "sunka"
+    },
+    "proizvod": null
+  },
+  {
+    "id": 2,
+    "kolicina": 100,
+    "sastojak": {
+      "id": 1,
+      "naziv": "sunka"
+    },
+    "proizvod": null
+  },
+  {
+    "id": 3,
+    "kolicina": 100,
+    "sastojak": {
+      "id": 1,
+      "naziv": "sunka"
+    },
+    "proizvod": null
+  },
+  {
+    "id": 14,
+    "kolicina": 100,
+    "sastojak": {
+      "id": 2,
+      "naziv": "salata"
+    },
+    "proizvod": null
+  },
+  {
+    "id": 15,
+    "kolicina": 100,
+    "sastojak": {
+      "id": 3,
+      "naziv": "krastavac"
+    },
+    "proizvod": null
+  },
+  {
+    "id": 16,
+    "kolicina": 30,
+    "sastojak": {
+      "id": 3,
+      "naziv": "krastavac"
+    },
+    "proizvod": null
+  },
+  {
+    "id": 17,
+    "kolicina": 30,
+    "sastojak": {
+      "id": 3,
+      "naziv": "krastavac"
+    },
+    "proizvod": null
+  },
+  {
+    "id": 18,
+    "kolicina": 30,
+    "sastojak": {
+      "id": 3,
+      "naziv": "krastavac"
+    },
+    "proizvod": null
+  }
+]*/
--- Template/Live Server/Prodavnica.js
+++ Template/Live Server/Prodavnica.js
@@ -64,9 +64,25 @@ export class Prodavnica{
         dugme.innerHTML="poruci";
         host.appendChild(dugme);
 
+        var dugmePoruci=document.createElement("button");
+        dugmePoruci.innerHTML="poruci ono sto nedostaje";
+        divPrikaz.appendChild(dugmePoruci);
+        var divPrikaz1=document.createElement("div");
+        divPrikaz1.className="divPrikaz1";
+        divPrikaz.appendChild(divPrikaz1);
+        
+
         dugme.onclick=(ev)=>{
             var izabranPr=this.kontejner.querySelector('select[name="selektProizvoda"]').value;
             var izabranaKolicina=this.kontejner.querySelector(".kolicina").value;
+
+           /* var trazenjeDivaZaPrikaz1=document.querySelector(".divPrikaz1");
+            console.log(trazenjeDivaZaPrikaz1);
+            (trazenjeDivaZaPrikaz1.parentNode).removeChild(trazenjeDivaZaPrikaz1);///nadje se div koji moze da se obrise 
+            
+            var divPrikaz1=document.createElement("div");
+                    divPrikaz1.className="divPrikaz1";
+                    divPrikaz.appendChild(divPrikaz1);*/
 
             fetch("https://localhost:5001/Ispit/Poruci/"+this.id+"/"+izabranPr+"/"+izabranaKolicina,
             {
@@ -74,16 +90,45 @@ export class Prodavnica{
                 headers:{
                     "Content-Type":"application/json"
                 },
-            }).then(p=>{if(p.ok){p.json().then(data=>{
+            }).then(p=>{if(p.ok)
+                {p.json().then(data=>{
                 data.forEach(elem => {
-                    var divSastojakKojiNedostaje=document.createElement("div");
-                    divSastojakKojiNedostaje.innerHTML=elem.naziv+" "+elem.kolicina;//////
-
-                    divPrikaz.appendChild(divSastojakKojiNedostaje);
+                   
+                    var SastojakKojiNedostaje=document.createElement("input");
+                  
+                    SastojakKojiNedostaje.type="checkbox";
+                    SastojakKojiNedostaje.value=elem.sastojak.id;
+                    SastojakKojiNedostaje.name="sastojciKojiNedostaju";
+                   
+                    let labela=document.createElement("label");
+                    labela.innerHTML=elem.sastojak.naziv+" "+elem.kolicina;//////
+                    divPrikaz1.appendChild(SastojakKojiNedostaje);
+                    divPrikaz1.appendChild(labela);
                 })
-            })
 
-            }
+                dugmePoruci.onclick=(ev)=>{
+                    var izabraniIDProizvoda=this.kontejner.querySelector('input[name="sastojciKojiNedostaju"]').value;
+
+                fetch("https://localhost:5001/Ispit/MenadzerPorucuje/"+this.id+"/"+izabraniIDProizvoda,//+"/"+izabranaKolicina,
+                {
+                method:"PUT",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                }).then(p=>{if(p.ok){
+                    alert("naruceni proizvod!");
+                    }
+                    else {
+                    alert("nije narucen proizvod");
+                    }
+                    })
+
+
+                    }
+                })
+
+                }
+            else {alert("uspesno narucena proizvod")}
             })
         }
     }
